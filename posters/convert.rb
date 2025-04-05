168f7d22f3fba65a3c97cd36cab7ad762058abc6
Michael McCourt <mkmccjr@gmail.com>
2025-04-04 19:06:02 -0700

    add posters page

--- posters/convert.rb
+++ posters/convert.rb
@@ -5,7 +5,7 @@ files = Dir.glob("*.pdf")
 
 files.each do |file|
   base = file.chomp(".pdf")
-  cmd = "gs -q -sDEVICE=png16m -dBATCH -dSAFER -dNOPAUSE -dFirstPage=1 -dLastPage=1 "
+  cmd = "gs -q -sDEVICE=pngalpha -sBATCH -dSAFER -sNOPAUSE "
   cmd += "-r#{res} "
   cmd += "-sOutputFile=#{base}.png #{base}.pdf"
   system cmd
